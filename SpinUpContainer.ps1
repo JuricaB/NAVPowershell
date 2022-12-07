b9d80638c30541de68f6bc9587883482af981fec
Jurica Bogunovic <JuricaB@users.noreply.github.com>
2022-12-07 15:28:33 +1300

    Add files via upload
    Moved license selection before getting artifact URL - ensures all user interaction is done before starting on container creation.

--- SpinUpContainer.ps1
+++ SpinUpContainer.ps1
@@ -7,15 +7,16 @@ if($ContainerName -eq "") {
 }
 
 do {
-$ContainerType = Read-Host "`n Sandbox (S) or Onprem (O)  "    
+$ContainerType = Read-Host "`n Sandbox (S) or Onprem (O)"    
 }
 until (($ContainerType -eq "S") -or ($ContainerType -eq "O"))
 
-if ($ContainerType -eq "S"){
+if (($ContainerType -eq "S")){
 $ContainerType = "Sandbox"
 } else {
 $ContainerType = "OnPrem"
 }
+
 
 $Version = Read-Host "`n Version (specific version or ENTER for latest)  "    
 
@@ -28,25 +29,13 @@ else
 $Select = 'Closest'
 }
 
-
-$Country = "nz"
-
-#Set password for ADMIN user to be same as container name
-$password = $containerName
-$securePassword = ConvertTo-SecureString -String $password -AsPlainText -Force
-$credential = New-Object pscredential 'admin', $securePassword
-$auth = 'UserPassword'
-if ($Version -eq ""){
-    $artifactUrl = Get-BcArtifactUrl -type $ContainerType -country $Country -select $Select
-}
-else {
-    $artifactUrl = Get-BcArtifactUrl -type $ContainerType -country $Country -select $Select -version $Version
-}
-Write-Host $artifactUrl
-
-
-
+Write-Host 'Select license:'
 $DefDir = 'C:\NAV\Other\'
+
+if ((Test-Path -Path $DefDir) -eq 0){
+    $DefDir = [System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::UserProfile) 
+    $DefDir = $DefDir + '\NAV\Other\'
+}
 if ((Test-Path -Path $DefDir) -eq 0){
     $DefDir = [Environment]::GetFolderPath('Desktop')
 }
@@ -61,6 +50,22 @@ if ((Test-Path -Path $LicenseFile -PathType Leaf) -eq 0){
     Write-Host "You must select a license - this is not valid" 
     EXIT
 }
+Write-Host 'License selected:' + $licenseFile
+
+$Country = "nz"
+
+#Set password for ADMIN user to be same as container name
+$password = $containerName
+$securePassword = ConvertTo-SecureString -String $password -AsPlainText -Force
+$credential = New-Object pscredential 'admin', $securePassword
+$auth = 'UserPassword'
+if ($Version -eq ""){    
+    $artifactUrl = Get-BcArtifactUrl -type $ContainerType -country $Country -select $Select    
+}
+else {
+    $artifactUrl = Get-BcArtifactUrl -type $ContainerType -country $Country -select $Select -version $Version
+}
+Write-Host 'Artifact URL: ' + $artifactUrl
 
 New-BcContainer `
     -accept_eula `
@@ -68,10 +73,12 @@ New-BcContainer `
     -credential $credential `
     -auth $auth `
     -artifactUrl $artifactUrl `
+    -isolation process `
     -imageName 'myimage' `
     -assignPremiumPlan `
     -licenseFile $licenseFile `
     -includeAL `
     -vsixFile (Get-LatestAlLanguageExtensionUrl) `
     -updateHosts `
-    -includeTestToolkit
+    -includeTestToolkit #'
+    #-includeCSide
